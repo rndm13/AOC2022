1045b9064054cc55565044fc407a783b50ee023a
rndm <alexbaranov47@gmail.com>
2022-12-04 17:24:00 +0200

    Played around with arrows for day 4

--- day_4/main.hs
+++ day_4/main.hs
@@ -1,29 +1,29 @@
-import qualified Control.Monad as M
-import qualified Data.List.Split as LS
+import           Control.Arrow
+import qualified Control.Monad    as M
+import qualified Data.List.Split  as LS
+import qualified Data.Tuple.Extra as E
 
-toTuple :: [a] -> (a,a)
-toTuple = (,) <$> head <*> last
+readRangePair :: String -> [[Int]]
+readRangePair str = (read <$>) <$> ranges
+  where ranges = LS.splitWhen ('-' ==) <$> LS.splitWhen (',' ==) str
 
-readRangePair :: String -> ((Int, Int), (Int, Int))
-readRangePair str = toTuple $ toTuple <$> ((read :: String -> Int) <$>) <$> ranges
-  where
-    ranges = LS.splitWhen ('-' ==) <$> LS.splitWhen (',' ==) str
-
-contains :: (Int, Int) -> (Int, Int) -> Bool
-contains (al, ar) (bl, br) =
+contains :: [[Int]] -> Bool
+contains [[al, ar], [bl, br]] =
   (al <= bl && br <= ar) || (bl <= al && ar <= br)
 
-overlaps :: (Int, Int) -> (Int, Int) -> Bool
-overlaps (al, ar) (bl, br)
+overlaps :: [[Int]] -> Bool
+overlaps [[al, ar], [bl, br]]
   | bl <= ar && ar <= br = True
   | al <= br && br <= ar = True
-  | otherwise = contains (al, ar) (bl, br)
+  | otherwise = contains [[al, ar], [bl, br]]
 
 count :: (a -> Bool) -> [a] -> Int
-count f = length . filter f
+count f arr = foldr (\x s -> if (f x) then (s + 1) else s) 0 arr
+
+toBoth :: Arrow a => a c d -> a b (c, c) -> a b (d, d)
+toBoth toAdd orig = orig >>> (toAdd *** toAdd)
 
 main :: IO ()
 main = do
-  input <- (readRangePair <$>) <$> lines <$> getContents 
-  print . count (uncurry contains) $ input -- Part 1
-  print . count (uncurry overlaps) $ input -- Part 2
+  input <- (readRangePair <$>) <$> lines <$> getContents
+  print . ((E.both sum) . unzip . (toBoth fromEnum (contains &&& overlaps) <$>)) $ input -- Part 1 and Part 2
