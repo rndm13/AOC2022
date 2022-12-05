fc9310c8b3003d6c9be383b0bff757d94bb80b32
rndm <alexbaranov47@gmail.com>
2022-12-05 16:48:59 +0200

    More readable day 5

--- day_5/main.hs
+++ day_5/main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE TemplateHaskell, RankNTypes, DerivingStrategies #-}
-
 module Main where
-
 import qualified Data.List.Split as LS
 import qualified Data.List       as L
 import Data.Char (isAlpha)
 import Control.Lens
+import Control.Arrow
 
 data Crate = Crate Char
            | EmptySpace
@@ -18,7 +17,7 @@ instance Read Crate where
       else [(Crate (cur !! 1), rest)]
         where 
           cur  = take 3 input
-          rest = drop 4 input
+          rest = drop 3 input
 
 instance Show Crate where
   show (Crate c) = [c]
@@ -32,22 +31,30 @@ safeHead []  = Nothing
 safeHead arr = Just (head arr)
 
 doMoveWith :: ([Crate] -> [Crate]) -> [Int] -> [[Crate]] -> [[Crate]]
-doMoveWith func [count, from, to] crates = newCrates
-  where
-    toMove = take count (crates !! (from - 1))
-    newCrates = crates & (over (ix (from - 1)) (drop count)) & (over (ix (to - 1)) (func toMove ++))
+doMoveWith func [count, from, to] crates = 
+  (over (ix (to - 1)) (func toMove ++))
+  . (over (ix (from - 1)) (drop count)) $ crates
+    where toMove = take count (crates !! (from - 1))
 
 solveWith :: ([Int] -> [[Crate]] -> [[Crate]]) -> [[Crate]] -> [[Int]] -> [[Crate]]
 solveWith mover crates moves = foldr (mover) crates (reverse moves)
 
 printTops :: [[Crate]] -> IO()
-printTops = putStrLn . concat . (map (show . maybe EmptySpace id . safeHead))
+printTops = putStrLn . concat
+          . (show . maybe EmptySpace id . safeHead <$>)
 
 main :: IO ()
 main = do
-  input <- ((,) <$> (reverse . (map (L.dropWhile (==EmptySpace))) . L.transpose . 
-                    ((map ((map (read :: String -> Crate)) . reverse . LS.chunksOf 4)) . dropr 1 . head))
-                <*> (map (map (read :: String -> Int) . filter (not . null) . LS.splitWhen (==' ') . filter (not . isAlpha))) . last)
+  input <- ((,) --- Parsing the crate stacks
+                <$> (reverse . (map (L.dropWhile (==EmptySpace))) . L.transpose 
+                 .  (map (map (read :: String -> Crate) . reverse . LS.chunksOf 4))
+                 .  dropr 1 . head)
+                --- Parsing moves
+                <*> (map (map (read :: String -> Int)
+                 .  filter (not . null) . LS.splitWhen (==' ')
+                 .  filter (not . isAlpha))) . last)
+                --- Reading input -> Splitting to lines -> Splitting at the empty line
                       . LS.splitWhen null . lines <$> getContents
+                --- In the end we get a tuple (Crate stacks, Moves)
   printTops . (uncurry (solveWith $ doMoveWith reverse)) $ input
   printTops . (uncurry (solveWith $ doMoveWith id)) $ input
